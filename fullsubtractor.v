5bdc834e9095a17100d3639d2f5283699867958b
Denisska1231 <90751218+Denisska1231@users.noreply.github.com>
2022-10-12 12:48:45 +0700

    fixed fullsub

--- fullsubtractor.v
+++ fullsubtractor.v
@@ -1,7 +1,9 @@
-module subtractor6(A,B,D,Br,Bout)
-    input [5:0]A,[5:0]B;
-    output [5:0]D,[4:0]Br;
-    fullsubtractor f0(D[0],Br[0],A[0],B[0],1'b0);
+module subtractor6(D,Bout,A,B);
+    input [5:0]A,B;
+    output [5:0]D;
+    output Bout;
+    wire [4:0]Br;
+    fullsubtractor f0(D[0],Br[0],A[0],B[0],1'd0);
     fullsubtractor f1(D[1],Br[1],A[1],B[1],Br[0]);
     fullsubtractor f2(D[2],Br[2],A[2],B[2],Br[1]);
     fullsubtractor f3(D[3],Br[3],A[3],B[3],Br[2]);
@@ -10,16 +12,16 @@ module subtractor6(A,B,D,Br,Bout)
 
 endmodule
 
-module fullsubtractor(A,B,C,D,Br)
+module fullsubtractor(D,Br,A,B,C);
     input A,B,C;
     output D,Br;
-    wire AxorB,A',A'andB,AxorB',CandAxorB';
+    wire AxorB,An,AnandB,AxorBn,CandAxorBn;
 
     xor x1(AxorB,A,B);
-    not n1(A',A);
-    and a1(A'andB,A',B);
-    not n2(AxorB',AxorB);
+    not n1(An,A);
+    and a1(AnandB,An,B);
+    not n2(AxorBn,AxorB);
     xor x2(D,AxorB,C);
-    and a2(CandAxorB',C,AxorB');
-    or  o1(Br,CandAxor,A'andB);    
+    and a2(CandAxorBn,C,AxorBn);
+    or  o1(Br,CandAxorBn,AnandB);    
 endmodule
